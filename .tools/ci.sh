a7c34a8343350451236b7473c7cb062ab4cc8407
Oleh Prypin <oleh@pryp.in>
2020-11-22 23:28:47 +0100

    Add CI and tools

---
+++ .tools/ci.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+cd "$(dirname "$0")/.."
+
+with_groups() {
+    echo "::group::$@"
+    "$@" && echo "::endgroup::"
+}
+
+"$@" autoflake -i -r --remove-all-unused-imports --remove-unused-variables mkdocs_same_dir
+"$@" isort -q mkdocs_same_dir
+"$@" black -q mkdocs_same_dir
+"$@" mkdocs build -f example/mkdocs.yml -q --strict
+"$@" grep -q adjacent site/index.html
